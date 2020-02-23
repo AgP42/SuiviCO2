c8b46d5b9b58916af65c5d75a50969589888dd59
Agathe Pinel <agp.com@mailbox.org>
2020-02-23 16:46:33 +0100

    debut modif panel

--- desktop/js/panel.js
+++ desktop/js/panel.js
@@ -24,21 +24,24 @@ $('#bt_validChangeDateSuiviCO2').on('click', function () {
   $('#div_chartConsokWh').packery('destroy');
   $('#div_chartConsoCO2').packery('destroy');
   displayGraphsCO2(eqLogic_id, $('#in_startDate').value(), $('#in_endDate').value());
+
+//  $('#div_alert').showAlert({message: $('#in_startDate').value(), level: 'info'});
 });
 
-//$('#div_alert').showAlert({message: eqLogic_id, level: 'info'});
+
+//setTimeout(function(){ displayGraphsCO2(eqLogic_id); }, 1); //TODO a tester
 
 /* Fonctions pour afficher les graphs */
 displayGraphsCO2(eqLogic_id,'',''); // appelle la fonction ci-dessous au lancement de la page. eqLogic_id vient du php
 
-function displayGraphsCO2(eqLogic_id,_dateStart,_dateEnd) {
+function displayGraphsCO2(_eqLogic_id, _dateStart, _dateEnd) {
   // l'appel ajax pour aller chercher les données selon l'équipement selectionné
   $.ajax({
     type: 'POST',
     url: 'plugins/suiviCO2/core/ajax/suiviCO2.ajax.php',
     data: {
       action: 'getSuiviCO2Data',
-      eqLogic_id: eqLogic_id,
+      eqLogic_id: _eqLogic_id,
       dateStart : _dateStart,
       dateEnd : _dateEnd,
     },
@@ -67,7 +70,30 @@ function displayGraphsCO2(eqLogic_id,_dateStart,_dateEnd) {
       graphCO2(data.result.eqLogic.id);
 
       // appel la construction du graphe conso Wh (en haut à droite)
-      graphConso(data.result.eqLogic.id);
+      //graphConso(data.result.eqLogic.id);
+      var series = []
+
+      // calcul pour le graph en haut a droite "run time by day"
+      series.push({
+        step: true,
+        name: data.result.eqLogic.name,
+        data: data.result.runtimeByDay,
+        type: 'column',
+        stack : 1,
+        unite : 'Wh',
+        stacking : 'normal',
+        dataGrouping: {
+            approximation: "sum",
+            enabled: true,
+            forced: true,
+            units: [[groupBy,[1]]]
+        },
+        tooltip: {
+            valueDecimals: 2
+        },
+      });
+
+      drawSimpleGraph('div_chartConsokWh', series, 'column');
 
     } // fin success
   }); //fin appel ajax
@@ -90,9 +116,13 @@ function graphCO2(_eqLogic_id) {
             dateStart: $('#in_startDate').value(),
             dateEnd: $('#in_endDate').value(),
             option: {
-              graphColor: '#' + Math.floor(Math.random()*16777215).toString(16),
-              derive : 0,
-              graphZindex : 3
+              graphColor: '#27ae60'
+            //  graphColor: '#' + Math.floor(Math.random()*16777215).toString(16),
+            //  derive : 0,
+            //  graphStep: 1,
+            //  graphScale : 1,
+            //  graphType : 'area',
+            //  graphZindex :1
             }
           });
         } //fin if courbe
@@ -150,4 +180,87 @@ function graphConso(_eqLogic_id) {
       }, 1000);
     }// fin success
   }); // fin jeedom.eqLogic.getCmd
-} // fin fct graphCO2
+} // fin fct graphConso
+
+function drawSimpleGraph(_el, _serie) {
+  new Highcharts.StockChart({
+    chart: {
+      zoomType: 'x',
+      renderTo: _el,
+      height: 180,
+      spacingTop: 0,
+      spacingLeft: 0,
+      spacingRight: 0,
+      spacingBottom: 0
+    },
+    credits: {
+      text: '',
+      href: '',
+    },
+    navigator: {
+      enabled: false
+    },
+    rangeSelector: {
+      buttons: [{
+        type: 'minute',
+        count: 30,
+        text: '30m'
+      }, {
+        type: 'hour',
+        count: 1,
+        text: 'H'
+      }, {
+        type: 'day',
+        count: 1,
+        text: 'J'
+      }, {
+        type: 'week',
+        count: 1,
+        text: 'S'
+      }, {
+        type: 'month',
+        count: 1,
+        text: 'M'
+      }, {
+        type: 'year',
+        count: 1,
+        text: 'A'
+      }, {
+        type: 'all',
+        count: 1,
+        text: 'Tous'
+      }],
+      selected: 6,
+      inputEnabled: false
+    },
+    legend: {
+      enabled: false
+    },
+    tooltip: {
+      pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} {{minute(s)}}</b><br/>',
+      valueDecimals: 2,
+    },
+    yAxis: {
+      format: '{value}',
+      showEmpty: false,
+      showLastLabel: true,
+      min: 0,
+      labels: {
+        align: 'right',
+        x: -5
+      }
+    },
+    scrollbar: {
+      barBackgroundColor: 'gray',
+      barBorderRadius: 7,
+      barBorderWidth: 0,
+      buttonBackgroundColor: 'gray',
+      buttonBorderWidth: 0,
+      buttonBorderRadius: 7,
+      trackBackgroundColor: 'none', trackBorderWidth: 1,
+      trackBorderRadius: 8,
+      trackBorderColor: '#CCC'
+    },
+    series: _serie
+  });
+}
--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -16,7 +16,28 @@ if (!is_object($object)) {
 $date = array(
   'start' => init('startDate', date('Y-m-d', strtotime('-1 month ' . date('Y-m-d')))),
   'end' => init('endDate', date('Y-m-d', strtotime('+1 days ' . date('Y-m-d')))),
-);
+);//*/
+
+sendVarToJs('groupBy', init('groupBy', 'day'));
+
+/*if (init('groupBy', 'month') == 'day') {
+  $date = array(
+    'start' => init('startDate', date('Y-m-d', strtotime('-31 days ' . date('Y-m-d')))),
+    'end' => init('endDate', date('Y-m-d')),
+  );
+}
+if (init('groupBy', 'month') == 'month') {
+  $date = array(
+    'start' => init('startDate', date('Y-m-d', strtotime('-1 year ' . date('Y-m-d')))),
+    'end' => init('endDate', date('Y-m-d', strtotime('+1 days' . date('Y-m-d')))),
+  );
+}
+if (init('groupBy', 'month') == 'year') { //TODO
+  $date = array(
+    'start' => init('startDate', date('Y-m-d', strtotime('-1 year ' . date('Y-m-d')))),
+    'end' => init('endDate', date('Y-m-d', strtotime('+1 days' . date('Y-m-d')))),
+  );
+} //*/
 
 if (is_object($object)) {
   $_GET['object_id'] = $object->getId();
@@ -63,15 +84,47 @@ sendVarToJs('eqLogic_id', init('eqLogic_id'));
     <div>
       <legend style="height: 35px;">
         <span class="objectName"></span>
-        <span class="pull-right">
-          {{Du}} <input class="form-control input-sm in_datepicker" id='in_startDate' style="display : inline-block; width: 150px;" value='<?php echo $date['start'] ?>'/> {{au}}
+
+           {{Période du}} <input class="form-control input-sm in_datepicker" id='in_startDate' style="display : inline-block; width: 150px;" value='<?php echo $date['start'] ?>'/> {{au}}
           <input class="form-control input-sm in_datepicker" id='in_endDate' style="display : inline-block; width: 150px;" value='<?php echo $date['end'] ?>'/>
           <a class="btn btn-success btn-sm tooltips" id='bt_validChangeDateSuiviCO2' title="{{Attention une trop grande plage de date peut mettre très longtemps a être calculée ou même ne pas s'afficher}}">{{Ok}}</a>
-        </span>
+
+          {{grouper par : }}
+
+          <!-- TODO a gerer en JS plutot d'en lien, pour pas tout recharger et perdre les dates demandées par l'user...-->
+          <?php
+          if (init('groupBy', 'day') == 'day') {
+            echo '<a class="btn btn-primary btn-sm" href="index.php?v=d&m=suiviCO2&p=panel&groupBy=day&eqLogic_id=' . $eqLogic->getId() . '">{{Jour}}</a> ';
+          } else {
+            echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=suiviCO2&p=panel&groupBy=day&eqLogic_id=' . $eqLogic->getId() . '">{{Jour}}</a> ';
+          }
+          if (init('groupBy', 'day') == 'month') {
+            echo '<a class="btn btn-primary btn-sm" href="index.php?v=d&m=suiviCO2&p=panel&groupBy=month&eqLogic_id=' . $eqLogic->getId() . '">{{Mois}}</a> ';
+          } else {
+            echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=suiviCO2&p=panel&groupBy=month&eqLogic_id=' . $eqLogic->getId() . '">{{Mois}}</a> ';
+          }
+          if (init('groupBy', 'day') == 'year') {
+            echo '<a class="btn btn-primary btn-sm" href="index.php?v=d&m=suiviCO2&p=panel&groupBy=year&eqLogic_id=' . $eqLogic->getId() . '">{{Année}}</a> ';
+          } else {
+            echo '<a class="btn btn-default btn-sm" href="index.php?v=d&m=suiviCO2&p=panel&groupBy=year&eqLogic_id=' . $eqLogic->getId() . '">{{Année}}</a> ';
+          }
+          ?>
+
+          <!-- TODO -->
+          <span class="pull-right">
+            <span class='label label-success' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="co2"></span> CO2</span>
+            <span class='label label-primary' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="consumption"></span> kWh</span>
+            <span class='label label-default' style="font-size: 0.9em;"><span class="energyAttr" data-l1key="total" data-l2key="cost"> </span> €</span>
+          </span>
+
       </legend>
     </div>
 
-    <!-- premiere moitier haute avec 2 graphs en ligne -->
+    <!-- moitier haute avec 1 graphs -->
+    <legend><i class="fas fa-bolt"></i>  {{Mes émissions gCO2}}</legend>
+    <div id="div_chartConsoCO2"></div>
+
+    <!-- moitier basse avec 2 graphs en ligne -->
     <div class="row">
       <div class="col-lg-6">
         <legend><i class="fas fa-leaf"></i>  {{gCO2 émis par kWh en France}}</legend>
@@ -84,9 +137,6 @@ sendVarToJs('eqLogic_id', init('eqLogic_id'));
     </div>
 
     <br/>
-    <!-- seconde moitier basse avec 1 graphs -->
-    <legend><i class="fas fa-bolt"></i>  {{Mes émissions gCO2}}</legend>
-    <div id="div_chartConsoCO2"></div>
 
   </div>
 
